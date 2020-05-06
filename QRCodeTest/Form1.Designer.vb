cdb070c48fbcda4a65a3eaa7adbb7f29f926f351
Neil Warren <neil.p.warren@gmail.com>
2020-05-06 12:23:08 -0700

    First Commit

---
+++ QRCodeTest/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.pagesUpDown = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.labelComboBox = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.pagesUpDown, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(206, 50)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 20)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'pagesUpDown
+        '
+        Me.pagesUpDown.Location = New System.Drawing.Point(111, 50)
+        Me.pagesUpDown.Name = "pagesUpDown"
+        Me.pagesUpDown.Size = New System.Drawing.Size(75, 20)
+        Me.pagesUpDown.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "# of Pages"
+        '
+        'labelComboBox
+        '
+        Me.labelComboBox.FormattingEnabled = True
+        Me.labelComboBox.Location = New System.Drawing.Point(111, 12)
+        Me.labelComboBox.Name = "labelComboBox"
+        Me.labelComboBox.Size = New System.Drawing.Size(170, 21)
+        Me.labelComboBox.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(60, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Label Type"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(293, 85)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.labelComboBox)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pagesUpDown)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "QR Code Generator"
+        CType(Me.pagesUpDown, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents pagesUpDown As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents labelComboBox As ComboBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ QRCodeTest/Form1.vb
@@ -0,0 +1,74 @@
+﻿Imports Microsoft.Office.Interop
+
+Public Class Form1
+    Dim oApp As Word.Application
+    Dim oDoc As Word.Document
+    Dim oDataFileName As String
+    Dim numLabelPerPage As New ArrayList()
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim oAutoText As Word.AutoTextEntry
+        oApp = CreateObject("Word.Application")
+        oDoc = oApp.Documents.Add
+
+        oDoc.Fields.Add(Range:=oApp.Selection.Range,
+                        Type:=Word.WdFieldType.wdFieldMergeBarcode,
+                        Text:="qrcode QR \s 55 \q 3",
+                        PreserveFormatting:=False)
+
+        oAutoText = oApp.NormalTemplate.AutoTextEntries.Add("MyLabelLayout", oDoc.Content)
+        oDoc.Content.Delete()
+        CreateMailMergeDataFile()
+
+        With oDoc.MailMerge
+            .MainDocumentType = Word.WdMailMergeMainDocType.wdMailingLabels
+            .OpenDataSource(Name:=oDataFileName)
+
+            oApp.MailingLabel.CreateNewDocument(Name:="8160 Address Labels", Address:="",
+                                                AutoText:="MyLabelLayout", LaserTray:=Word.WdPaperTray.wdPrinterManualFeed)
+            .Destination = Word.WdMailMergeDestination.wdSendToNewDocument
+
+            .Execute()
+
+        End With
+
+        oAutoText.Delete()
+        oDoc.Close(False)
+        oApp.Visible = True
+
+        My.Computer.FileSystem.DeleteFile(oDataFileName)
+        oApp.NormalTemplate.Saved = True
+
+    End Sub
+
+    Public Sub CreateMailMergeDataFile()
+        Dim iNumLabels As Integer
+        Dim oDataDoc As Word.Document
+        Dim qrCode As Guid
+        iNumLabels = pagesUpDown.Value * numLabelPerPage(labelComboBox.SelectedIndex)
+
+        oDoc.MailMerge.CreateDataSource(Name:=oDataFileName, HeaderRecord:="qrcode")
+        oDataDoc = oApp.Documents.Open(oDataFileName)
+
+        For iCount = 1 To iNumLabels
+            With oDataDoc.Tables.Item(1)
+                qrCode = Guid.NewGuid()
+                If iCount <> iNumLabels Then
+                    .Rows.Add()
+                End If
+                .Cell(iCount + 1, 1).Range.InsertAfter(qrCode.ToString())
+            End With
+        Next
+
+        oDataDoc.Save()
+        oDataDoc.Close(False)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        oDataFileName = String.Format("{0}\labelDataSource.docx", Environment.CurrentDirectory)
+        labelComboBox.Items.Add("8160 Address Labels")
+        numLabelPerPage.Add(30)
+
+        labelComboBox.SelectedIndex = 0
+        pagesUpDown.Value = 1
+    End Sub
+End Class
